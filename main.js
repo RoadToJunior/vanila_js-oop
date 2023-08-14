daa27684ef852c7c3241e1be8a169e42ad389eae
NowWeAre-Free <kontodarm@wp.pl>
2023-08-14 16:54:28 +0200

    konstruktory i instancje

--- main.js
+++ main.js
@@ -7,12 +7,26 @@
 
 // class
 
-class Animal {
-  constructor(name, species) {
-    this.name = name;
-    this.species = species;
-    this.eat = function () {
-      console.log(this.name + " mniam mniam");
-    };
-  }
+// class Animal {
+//   constructor(name, species) {
+//     this.name = name;
+//     this.species = species;
+//     this.eat = function () {
+//       console.log(this.name + " mniam mniam");
+//     };
+//   }
+// }
+
+//pierwszy sposób na utworzenie konstruktora
+const Car = function (name) {
+  this.name = name;
+};
+
+//drugi sposób
+function Car(name) {
+  this.name = name;
 }
+
+//instacje
+const maluch = new Car("maluch");
+const polonez = new Car("polonez");
